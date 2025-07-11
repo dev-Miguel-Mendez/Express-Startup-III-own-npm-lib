c555fc6a9f332679e92c792088a5809f3805a836
Priapisman677 <expeliamm02@hotmail.com>
2025-07-11 09:16:53 -0700

    update

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -14,19 +14,19 @@ test('Should load test.env', ()=>{
 test('Should not fail for /signup', async() => {
     const result = await request(app).post('/api/signup').send({ username: 'Dwati', email: 'Dwati@email.com', password: '12345678' });
     
-    expect(result.body).toEqual({message: 'Success', data: { username: 'Dwati', email: 'Dwati@email.com', password: '12345678' }});
+    expect(result.body).toEqual({message: 'Success', success: true, data: { username: 'Dwati', email: 'Dwati@email.com', password: '12345678' }});
 });
 
 test('Should not fail for /login', async() => {
     const result = await request(app).post('/api/signin').send({ email: 'Dwati@email.com', password: '12345678' });
     
-    expect(result.body).toEqual({message: 'Success', data: { email: 'Dwati@email.com', password: '12345678' }});
+    expect(result.body).toEqual({message: 'Success', success: true, data: { email: 'Dwati@email.com', password: '12345678' }});
 });
 
 
 test('Should fail for /signup', async() => {
     const result = await request(app).post('/api/signup').send({ username: 'abc', email: 'agugutata', password: '123' });
-    
+    expect(result.body.success).toBe(false);
     expect(result.body.message[0]).toBe("Invalid email");
     expect(result.body.message[1]).toBe("Password must be at least 6 characters long");
 });
